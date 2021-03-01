e5968575e1297fc57c05ab7454a008db06b8a2ba
fritzgt <fritzgt@icloud.com>
2021-03-01 10:10:20 +0200

    Initial Commit

---
+++ MVVM iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM iOS
+//
+//  Created by FGT MAC on 3/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
